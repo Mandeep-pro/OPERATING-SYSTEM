77f817425064ce6a4deacd4a2eb5b0143eb11493
Mandeep007 <mandeepsinghabd000@gmail.com>
2026-05-27 23:19:51 +0530

    Add MFU page replacement algorithm
    Implement MFU page replacement algorithm in C.

---
+++ Mfu.c
@@ -0,0 +1,33 @@
+// MFU Page Replacement Algorithm in C
+#include <stdio.h>
+
+int main() {
+    int n, f, fault = 0;
+    printf("Pages: "); scanf("%d", &n); int seq[n];
+    for(int i=0; i<n; i++) scanf("%d", &seq[i]);
+    
+    printf("Frames: "); 
+    scanf(" %d", &f); 
+    
+    int frame[f], freq[f];
+    for(int i=0; i<f; i++) { frame[i] = -1; freq[i] = 0; }
+
+    for(int i=0; i<n; i++) {
+        int found = 0, pos = -1;
+        for(int j=0; j<f; j++) {
+            if(frame[j] == seq[i]) { found = 1; pos = j; break; }
+            if(frame[j] == -1 && pos == -1) pos = j;
+        }
+        if(!found) {
+            fault++;
+            if(pos == -1) {
+                int max_idx = 0;
+                for(int j=1; j<f; j++) if(freq[j] > freq[pos]) max_idx = j;
+                pos = max_idx;
+            }
+            freq[pos] = 0;
+        }
+        frame[pos] = seq[i]; freq[pos]++;
+    }
+    printf("Faults: %d\n", fault);
+}
